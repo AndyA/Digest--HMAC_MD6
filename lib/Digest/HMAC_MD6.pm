03645466c4e4f6261a599edd76d685300958742b
Andy Armstrong <andy@hexten.net>
2009-06-18 09:38:54 +0100

    Initial checkin

---
+++ lib/Digest/HMAC_MD6.pm
@@ -0,0 +1,62 @@
+package Digest::HMAC_MD6;
+
+use warnings;
+use strict;
+
+use Carp;
+
+=head1 NAME
+
+Digest::HMAC_MD6 - MD6 Keyed-Hashing for Message Authentication
+
+=head1 VERSION
+
+This document describes Digest::HMAC_MD6 version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+  use Digest::HMAC_MD6;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< new >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+Digest::HMAC_MD6 requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+Please report any bugs or feature requests to
+C<bug-digest-hmac_md6@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2009, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
